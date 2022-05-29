1b3de476bdec360001bd3c7f2fd24c27b0be31a2
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-28 18:48:46 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+//importing the files from the index components
 import { Download, Features, SectionWrapper } from "./components";
 
 const App = () => {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold ">React Nft</h1>
+      <Download />
+      <Features />
+      <SectionWrapper />
     </div>
   );
 }
