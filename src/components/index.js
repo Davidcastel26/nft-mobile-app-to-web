f25be67b132ee51f04f4e03952b9d334e6ecaaa2
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-28 18:45:26 -0600

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Download from './components/Download.jsx'
+import Features from './components/Features.jsx'
+import SectionWrapper from './components/SectionWrapper.jsx'
+
+export { Features, Download, SectionWrapper }
