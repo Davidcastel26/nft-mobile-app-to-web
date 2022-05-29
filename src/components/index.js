585b260690a6cedfb7e2ae8f4f9e3b627c164a83
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-28 18:47:27 -0600

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
-import Download from './components/Download.jsx'
-import Features from './components/Features.jsx'
-import SectionWrapper from './components/SectionWrapper.jsx'
+import Download from './Download'
+import Features from './Features'
+import Button from './Button'
+import SectionWrapper from './SectionWrapper'
 
-export { Features, Download, SectionWrapper }
+export { Features, Download, SectionWrapper, Button }
