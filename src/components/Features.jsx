8de91c00bf9927c0adf025f02a1a47f478e467d0
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-30 16:07:39 -0600

    Update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import assets from '../assets'
+import styles from '../styles/Global'
+
+const FeaturesCard = () => {
+  
+}
+
 const Features = () => {
   return (
-    <div>Features</div>
+    <div  className={`${styles.section} ${styles.bgPrimary} banner03`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div >
+          <h1 className={`${styles.whiteText} ${styles.h1Text}`} >Technologies</h1>
+          <p className={`${styles.whiteText} ${styles.pText}`}>ProNef has been developed using a cross-platform technology, React Native.</p>
+        </div>
+        <div className={`${styles.flexWrap}`}>
+          <FeaturesCard />
+        </div>
+      </div>
+    </div>
   )
 }
 
