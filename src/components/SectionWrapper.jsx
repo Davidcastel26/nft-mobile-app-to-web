36702c25b6cf839f441447b45a4b25d9b8be3646
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-30 12:45:30 -0600

    Update SectionWrapper.jsx

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import styles from '../styles/Global';
 import assets from '../assets';
+import Button from './Button'
 
 const SectionWrapper = ({title, description, showBtn, mockupImg, banner }) => {
   return (
     <div className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}>
-      <div className={`flex items-center ${styles.boxClass}`}></div>
-      <h1>{title}</h1>
-      <h1>{description}</h1>
-      <img src={mockupImg} alt="mockup" />
+      <div className={`flex items-center ${styles.boxClass}`}>
+        <div className={`${styles.descDiv} fadeLeftMini`}>
+          <h1 className={`${styles.h1Text}`}>{title}</h1>
+          <p className={`${styles.descriptionText}`}>{description}</p>
+          {
+            showBtn && (
+              <Button />
+            )
+          }
+        </div>
+      </div>
+      {/* <img src={mockupImg} alt="mockup" /> */}
       
     </div>
   )
