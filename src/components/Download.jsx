1b4ef7f5f627e61153d777f4af10676af801f8d5
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-30 18:23:10 -0600

    Update Download.jsx

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import styles from '../styles/Global';
+import assets from '../assets';
+
 const Download = () => {
   return (
-    <div>Download</div>
+    <div className={`${styles.section} ${styles.bgWhite}`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+
+      </div>
+    </div>
   )
 }
 
