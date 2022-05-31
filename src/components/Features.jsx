aa20342976852d31533834fae87b2e0c08bd0d49
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-30 18:14:21 -0600

    Update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 import assets from '../assets';
 import styles from '../styles/Global';
 
-const FeatureCard = ({iconUrl, iconText}) => {
-  <div className={`${styles.featureCard}`}>
-    <img src={iconUrl} alt="" className={`${styles.featureImg}`} />
-    <p className={`${styles.featureText}`}>{iconText}</p>
+const FeatureCard = ({ iconUrl, iconText }) =>  (
+  <div className={styles.featureCard}>
+    <img src={iconUrl} alt="icon" className={styles.featureImg} />
+    <p className={styles.featureText}>{iconText}</p>
   </div>
-}
+)
 
 const Features = () => {
   return (
-    <div  className={`${styles.section} ${styles.bgPrimary} banner03`}>
+    <div className={`${styles.section} ${styles.bgPrimary} banner03`}>
       <div className={`${styles.subSection} flex-col text-center`}>
-        <div >
-          <h1 className={`${styles.whiteText} ${styles.h1Text}`} >Technologies</h1>
-          <p className={`${styles.whiteText} ${styles.pText}`}>ProNef has been developed using a cross-platform technology, React Native.</p>
+        <div>
+          <h1 className={`${styles.h1Text} ${styles.whiteText}`}>Technologies</h1>
+          <p className={`${styles.pText} ${styles.whiteText}`}>ProNef has been developed using a cross-platform technology, React Native.</p>
         </div>
+
         <div className={styles.flexWrap}>
           <FeatureCard iconUrl={assets.react} iconText="React Native" />
           <FeatureCard iconUrl={assets.javascript} iconText="JavaScript" />
