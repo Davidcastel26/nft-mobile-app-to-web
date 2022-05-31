49cb7195c206696d7014b4edfd717948b7776b1a
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-30 18:33:47 -0600

    Update Download.jsx

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -7,7 +7,13 @@ const Download = () => {
   return (
     <div className={`${styles.section} ${styles.bgWhite}`}>
       <div className={`${styles.subSection} flex-col text-center`}>
-
+        <div>
+          <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the Source Code</h1>
+          <p className={`${styles.pText} ${styles.blackText}`}>Get the full source code on GitHub</p>
+          <button className={`${styles.btnPrimary}`}> 
+            <a href="https://github.com/Davidcastel26/nft-mobile-app-to-web">  Source Code </a>
+           </button>
+        </div>
       </div>
     </div>
   )
